739e5c5382b40f26f732b9de3cc3285f325e7f1e
Jawad Rashid <nokia.2003@gmail.com>
2016-09-04 21:40:28 +0500

    Changing Background on Load

--- js/index.js
+++ js/index.js
@@ -34,17 +34,59 @@ function calculateJumbotronHeight() {
   
 }
 var pixabayJSON = null;
+var resultPerPage = 20;
+
+function generateNumber(min, max) {
+  return Math.ceil(Math.random() * (max - min + 1) + min);
+}
 
 function changeBackground() {
+  if(pixabayJSON != null) {
+    if("hits" in pixabayJSON) {
+      var hits = pixabayJSON["hits"];
+      var len = hits.length;
+      var currentNum = generateNumber(1, len) - 1;
+      var currentImage = hits[currentNum];
+      if("webformatURL" in currentImage) {
+        var imageUrl = currentImage["webformatURL"];
+        var formatRegex = /_\d+\./;
+        if(formatRegex.test(imageUrl)) {
+          imageUrl = imageUrl.replace(formatRegex, '_960.');
+        }
+        console.log(imageUrl);
+        $('.jumbotron-main').css("background-image", "url("+imageUrl+")");  
+
+        
+      }
+      
+    }
+  }
+}
+
+function getBackgrounds() {
   if (pixabayJSON == null) {
     var key = "3232217-3470d235678827ed374977f54";
-    $.getJSON("https://pixabay.com/api/?key=3232217-3470d235678827ed374977f54&category=backgrounds&safesearch=true")
+    var category = "background";
+    var safeSearch = true;
+    var totalResults = 500;
+    var totalPages = totalResults / resultPerPage;
+    var currentPage = generateNumber(1, totalPages);
+
+    var url = "https://pixabay.com/api/?key="+key+"&category="+category+"&safesearch="+safeSearch+"&page="+currentPage;
+    try {
+      $.getJSON(url, function(data) {
+        pixabayJSON = data;
+        changeBackground();
+      });
+    } catch(err) {
+      console.log(err);
+    }
   }
 }
 
 $(document).ready(function() {
   calculateJumbotronHeight();
-  changeBackground();
+  getBackgrounds();
 
   $('[data-toggle="contact"]').tooltip({
     track: true
